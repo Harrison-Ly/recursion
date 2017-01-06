a9b4add991418fb87631fc8888c863149cacf627
Harrison-Ly <harriko@gmail.com>
2017-01-06 00:53:24 +0000

    fibonacci sequence

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,15 +1,18 @@
 def fibs(n)
-  a, b = 0 , 1
-  (n-1).times do
-    a, b = b, a + b
+  s = [0, 1]
+  (n-2).times do
+    s << s[-1] + s[-2]
   end
-  a
+  s[0..n]
 end
 
-def fibs_rec(n)
-  n < 2 ? n : fibs(n-1) + fibs(n-2)
+def fibs_rec(n, arr = [0, 1])
+  return arr[0] if n == 0
+  return arr if n == 2
+  arr << arr[-1] + arr[-2]
+  fibs_rec(n-1, arr)
 end
 
 
-puts fibs(10)
-puts fibs_rec(10)
+puts fibs(10).inspect
+puts fibs_rec(10).inspect
